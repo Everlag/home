741649de34510c6301e8f873cab0bab084476105
Everlag <kyleveb@gmail.com>
2017-08-12 21:44:06 -0600

    workspace janky chrome extension workaround
    We cannot trivially install these from the shell at provisioning
    time. Instead, we check on each i3-startup if the extensions are
    not installed then start chrome with those specific extension
    windows open.

--- workspace/i3-startup.sh
+++ workspace/i3-startup.sh
@@ -19,7 +19,32 @@ for i in $(seq 1 $count); do
 done
 
 # Chrome
-nohup chromium-browser --disable-smooth-scrolling &
+#
+# We open chrome with specific urls based off of whether or not
+# we can detect the presence of desired extensions in the Extension
+# directory for chromium.
+#
+# Hence, first boot will yield all the store pages to install the
+# extensions and future boots will not.
+extensions=(
+    'vimium https://chrome.google.com/webstore/detail/vimium/dbepggeogbaibhgnhhndojpepiihcmeb?hl=en'
+    'ublock-origin https://chrome.google.com/webstore/detail/ublock-origin/cjpalhdlnbpafiamejdnhcphjbkeiagm?hl=en'
+    'session-buddy https://chrome.google.com/webstore/detail/session-buddy/edacconmaakjimmfgnblocblbcdcpbko?hl=en'
+)
+to_open=''
+for i in "${extensions[@]}"; do
+    # Look for the string in the correct directory
+    search_string=$(echo ${i} | awk '{print $1;}')
+    url=$(echo ${i} | awk '{print $2;}')
+    found=$(grep -rF -o ${search_string} \
+        /home/vagrant/.config/chromium/Default/Extensions/ |
+        head -n1)
+    # Not found, add it to the urls to open
+    if [ -z ${found} ]; then
+        to_open="${to_open} ${url}"
+    fi
+done
+nohup chromium-browser --disable-smooth-scrolling ${to_open} &
 
 # Code
 nohup code &
